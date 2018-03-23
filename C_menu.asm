7633cc2380871fff5d4728b5935a36a22467c702
Lior-brumberg <34351694+Lior-brumberg@users.noreply.github.com>
2018-03-23 17:55:53 +0300

    added bio feature

--- C_menu.asm
+++ C_menu.asm
@@ -4,6 +4,7 @@ STACK 100h
 DATASEG
 ; --------------------------
 ; Your variables here
+user_answer db ?
 sec db ?
 encr_1 db 'What to encrypt?$'
 AskForKey db 'Whats the key?$'
@@ -22,8 +23,47 @@ choice3 db '3) About my creatore(Lior Brumberg)$'
 choice4 db '4) About me!(what i do, how i do it)$'
 choice5 db '5) Exit$'
 start_line db '/>$'
+;----   bio subsection   ----
+BioBuffer db '=======   bio   ======= $'
+MyName db 'NAME: Lior Brumberg $'
+MyBirthDay db 'BIRTH DATE: 20/05/2001 $'
+MyCity db 'HOMETOWN: HADERA $'
+HighSchool db 'HIGHSCHOOL: tichon hadeara $'
+;----   general subsection   ----
+buffer db '============================= $'
+
+
 ; --------------------------
 CODESEG
+proc InputNumber
+; the procedure gets the offset address of a variable
+	push bp
+	mov bp, sp
+	push ax
+	push bx
+	push cx
+	push dx
+	
+	xor ax, ax
+	xor bx, bx
+	xor cx, cx
+	xor dx, dx
+	
+	mov ah, 1h
+	int 21h 
+	sub al, '0'
+	mov bx, [bp + 4]
+	mov [bx], al
+	
+	pop dx
+	pop cx
+	pop bx
+	pop ax
+	pop bp
+	ret
+; the procedure inputs a number from 0-9 into the variable.
+endp  InputNumber
+
 proc WaitingSec ; procedure for waiting to new second
 	push ax	; put the registers in the stack
 	push cx
@@ -163,6 +203,50 @@ proc print_choices
 	pop ax
 	ret
 endp print_choices
+proc Biography
+; the procedure gets nothing
+	push ax
+	push bx
+	push cx
+	push dx 
+	
+	push offset BioBuffer
+	call Print 
+	pop ax
+	call NewLine
+	
+	push offset MyName
+	call print 
+	pop ax
+	call NewLine
+	
+	push offset MyBirthDay
+	call Print
+	pop ax
+	call NewLine
+	
+	push offset MyCity
+	call Print 
+	pop ax
+	call NewLine
+	
+	push offset HighSchool
+	call print 
+	pop ax
+	call NewLine
+	
+	push offset buffer
+	call Print
+	pop ax
+	call NewLine
+	
+	pop dx
+	pop cx
+	pop bx
+	pop ax
+	ret 
+; the procedure prints the biography msg's.
+endp Biography
 start:
 	mov ax, @data
 	mov ds, ax
@@ -175,14 +259,27 @@ start:
 	call think
 	push offset greeting
 	call print
+ask:	
 	call NewLine
 	call think
 	call print_choices
+	push offset user_answer
+	call InputNumber
+	call NewLine
+	cmp [user_answer], 3
+	je my_bio
+	jmp ask
+my_bio:
+	call biography
+	jmp ask
 ; --------------------------
 	
 exit:
 	mov ax, 4c00h
 	int 21h
 END start
+
+
+
 
 
